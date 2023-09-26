0432b6f9cdc8b15d17ff1f3e0b472ebbd80df7a6
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2023-09-26 11:39:35 +0300

    Change emailVerified User field with UserAttributeMappers (conditional trust email)

--- server-spi-private/src/main/java/org/keycloak/broker/provider/BrokeredIdentityContext.java
+++ server-spi-private/src/main/java/org/keycloak/broker/provider/BrokeredIdentityContext.java
@@ -42,6 +42,7 @@ public class BrokeredIdentityContext {
     private String username;
     private String modelUsername;
     private String email;
+    private boolean emailVerified;
     private String firstName;
     private String lastName;
     private String brokerSessionId;
@@ -122,6 +123,14 @@ public class BrokeredIdentityContext {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
     }
 
     public String getBrokerSessionId() {
