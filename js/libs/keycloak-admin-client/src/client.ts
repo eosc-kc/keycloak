874d7d0a12b3ea4bc9d3eca9959b4d12e52c321a
Andreas Kozadinos <koza-sparrow@hotmail.com>
2025-10-22 18:10:01 +0300

    SAML Federation UI RCIAM[331]

--- js/libs/keycloak-admin-client/src/client.ts
+++ js/libs/keycloak-admin-client/src/client.ts
@@ -18,6 +18,8 @@ import { UserStorageProvider } from "./resources/userStorageProvider.js";
 import { WhoAmI } from "./resources/whoAmI.js";
 import { Credentials, getToken } from "./utils/auth.js";
 import { defaultBaseUrl, defaultRealm } from "./utils/constants.js";
+import { IdentityFederations } from "./resources/identityFederations.js";
+
 
 export type RequestOptions = Omit<RequestInit, "signal">;
 
@@ -46,6 +48,7 @@ export class KeycloakAdminClient {
   public clientScopes: ClientScopes;
   public clientPolicies: ClientPolicies;
   public identityProviders: IdentityProviders;
+  public identityFederations: IdentityFederations;
   public components: Components;
   public serverInfo: ServerInfo;
   public whoAmI: WhoAmI;
@@ -84,6 +87,7 @@ export class KeycloakAdminClient {
     this.clientScopes = new ClientScopes(this);
     this.clientPolicies = new ClientPolicies(this);
     this.identityProviders = new IdentityProviders(this);
+    this.identityFederations = new IdentityFederations(this);
     this.components = new Components(this);
     this.authenticationManagement = new AuthenticationManagement(this);
     this.serverInfo = new ServerInfo(this);
