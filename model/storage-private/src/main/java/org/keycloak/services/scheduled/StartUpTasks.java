2aa3fd64fb31fc8d8c4f07bfe2603f287f03755a
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2025-10-22 14:30:17 +0300

    KEYCLOAK-19348 SAML/ OIDC IdP AutoUpdate

--- model/storage-private/src/main/java/org/keycloak/services/scheduled/StartUpTasks.java
+++ model/storage-private/src/main/java/org/keycloak/services/scheduled/StartUpTasks.java
@@ -1,7 +1,10 @@
 package org.keycloak.services.scheduled;
 
-import java.time.Instant;
 
+import java.time.Instant;
+import java.util.Map;
+
+import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.timer.ScheduledTask;
 import org.keycloak.timer.TimerProvider;
@@ -16,6 +19,7 @@ public class StartUpTasks implements ScheduledTask {
     public void run(KeycloakSession session) {
         TimerProvider timer = session.getProvider(TimerProvider.class);
         session.realms().getRealmsStream().forEach(realm -> {
+            session.getContext().setRealm(realm);
             realm.getSAMLFederations().stream().forEach(model -> {
                 if (model.getLastMetadataRefreshTimestamp() == null) {
                     model.setLastMetadataRefreshTimestamp(0L);
@@ -25,6 +29,12 @@ public class StartUpTasks implements ScheduledTask {
                 long delay = model.getLastMetadataRefreshTimestamp() + (model.getUpdateFrequencyInMins() * 60 * 1000) - Instant.now().toEpochMilli();
                 timer.schedule(taskRunner, delay > 60 * 1000 ? delay : 60 * 1000, model.getUpdateFrequencyInMins() * 60 * 1000, "UpdateFederation" + model.getInternalId());
                 logger.info("Initiating update task of federation with id: " + model.getInternalId());
+            });
+            session.identityProviders().getAllStream(Map.of(IdentityProviderModel.AUTO_UPDATE, "true"), null, null).forEach(idp -> {
+                AutoUpdateIdentityProviders autoUpdateProvider = new AutoUpdateIdentityProviders(idp.getAlias(), realm.getId());
+                ClusterAwareScheduledTaskRunner taskRunner = new ClusterAwareScheduledTaskRunner(session.getKeycloakSessionFactory(), autoUpdateProvider, Long.valueOf(idp.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000);
+                long delay = idp.getConfig().get(IdentityProviderModel.LAST_REFRESH_TIME) == null ? Long.parseLong(idp.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000 : Long.parseLong(idp.getConfig().get(IdentityProviderModel.LAST_REFRESH_TIME) + Long.parseLong(idp.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000) - Instant.now().toEpochMilli();
+                timer.schedule(taskRunner, delay > 60 * 1000 ? delay : 60 * 1000, Long.valueOf(idp.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000, realm.getId() + "_AutoUpdateIdP_" + idp.getAlias());
             });
         });
     }
--- services/src/main/java/org/keycloak/partialimport/IdentityProvidersPartialImport.java
+++ services/src/main/java/org/keycloak/partialimport/IdentityProvidersPartialImport.java
@@ -19,12 +19,16 @@ package org.keycloak.partialimport;
 
 import java.util.List;
 
+import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.models.utils.RepresentationToModel;
 import org.keycloak.representations.idm.IdentityProviderRepresentation;
 import org.keycloak.representations.idm.PartialImportRepresentation;
+import org.keycloak.services.scheduled.AutoUpdateIdentityProviders;
+import org.keycloak.services.scheduled.ClusterAwareScheduledTaskRunner;
+import org.keycloak.timer.TimerProvider;
 
 /**
  * PartialImport handler for Identity Providers.
@@ -66,12 +70,23 @@ public class IdentityProvidersPartialImport extends AbstractPartialImport<Identi
     @Override
     public void remove(RealmModel realm, KeycloakSession session, IdentityProviderRepresentation idpRep) {
         session.identityProviders().remove(getName(idpRep));
+        //case IdP is autoUpdated delete its schedule task
+        TimerProvider timer = session.getProvider(TimerProvider.class);
+        timer.cancelTask(realm.getId()+"_AutoUpdateIdP_" + idpRep.getAlias());
     }
 
     @Override
     public void create(RealmModel realm, KeycloakSession session, IdentityProviderRepresentation idpRep) {
         idpRep.setInternalId(KeycloakModelUtils.generateId());
-        session.identityProviders().create(RepresentationToModel.toModel(realm, idpRep, session));
+        IdentityProviderModel identityProvider = RepresentationToModel.toModel(realm, idpRep, session);
+        session.identityProviders().create(identityProvider);
+        if (Boolean.valueOf(identityProvider.getConfig().get(IdentityProviderModel.AUTO_UPDATE))) {
+            //case IdP is autoUpdated create its schedule task
+            TimerProvider timer = session.getProvider(TimerProvider.class);
+            AutoUpdateIdentityProviders autoUpdateProvider = new AutoUpdateIdentityProviders(identityProvider.getAlias(), realm.getId());
+            ClusterAwareScheduledTaskRunner taskRunner = new ClusterAwareScheduledTaskRunner(session.getKeycloakSessionFactory(), autoUpdateProvider, Long.parseLong(identityProvider.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000);
+            timer.schedule(taskRunner, Long.parseLong(identityProvider.getConfig().get(IdentityProviderModel.REFRESH_PERIOD)) * 1000, realm.getId()+"_AutoUpdateIdP_" + identityProvider.getAlias());
+        }
     }
 
 }
