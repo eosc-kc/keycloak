f039eb3b1e782d5f7b946330fbda93076938cca9
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2025-10-29 16:56:21 +0200

    RCIAM-860 Changes in account console and account rest service (backend changes and basic ui)

--- model/storage-private/src/main/java/org/keycloak/storage/datastore/DefaultMigrationManager.java
+++ model/storage-private/src/main/java/org/keycloak/storage/datastore/DefaultMigrationManager.java
@@ -49,6 +49,7 @@ import org.keycloak.migration.migrators.MigrateTo26_2_0;
 import org.keycloak.migration.migrators.MigrateTo26_3_0;
 import org.keycloak.migration.migrators.MigrateTo26_4_0;
 import org.keycloak.migration.migrators.MigrateTo26_4_3;
+import org.keycloak.migration.migrators.MigrateTo26_4_1_0_1;
 import org.keycloak.migration.migrators.MigrateTo2_0_0;
 import org.keycloak.migration.migrators.MigrateTo2_1_0;
 import org.keycloak.migration.migrators.MigrateTo2_2_0;
@@ -129,7 +130,8 @@ public class DefaultMigrationManager implements MigrationManager {
             new MigrateTo26_1_0(),
             new MigrateTo26_2_0(),
             new MigrateTo26_3_0(),
-            new MigrateTo26_4_0(),
+            new MigrateTo26_4_0(), 
+            new MigrateTo26_4_1_0_1(),
             new MigrateTo26_4_3()
     };
 
