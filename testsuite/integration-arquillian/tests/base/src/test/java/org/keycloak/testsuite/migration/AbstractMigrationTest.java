04d2599d7e11f9248cdd9a50a1f1fb6c36cce639
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2025-10-29 16:56:21 +0200

    RCIAM-860 Changes in account console and account rest service (backend changes and basic ui)

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/migration/AbstractMigrationTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/migration/AbstractMigrationTest.java
@@ -456,6 +456,12 @@ public abstract class AbstractMigrationTest extends AbstractKeycloakTest {
 
     protected void testMigrationTo26_4_0() {
         testSamlEncryptionAttributes(migrationRealm);
+        testMigrationTo26_4_1_0_1();
+    }
+
+    protected void testMigrationTo26_4_1_0_1() {
+        testNewAccountRoles(masterRealm);
+        testNewAccountRoles(migrationRealm);
     }
 
     private void testClientContainsExpectedClientScopes() {
@@ -666,7 +672,7 @@ public abstract class AbstractMigrationTest extends AbstractKeycloakTest {
         }
     }
 
-    protected void testViewGroups(RealmResource realm) {
+   protected void testViewGroups(RealmResource realm) {
         ClientRepresentation accountClient = realm.clients().findByClientId(ACCOUNT_MANAGEMENT_CLIENT_ID).get(0);
 
         ClientResource accountResource = realm.clients().get(accountClient.getId());
@@ -698,6 +704,18 @@ public abstract class AbstractMigrationTest extends AbstractKeycloakTest {
                             .collect(Collectors.toList()),
                     not(hasItem(legacyTermsAndConditionsAlias)));
         }
+    }
+
+     protected void testNewAccountRoles(RealmResource realm) {
+        ClientRepresentation accountClient = realm.clients().findByClientId(ACCOUNT_MANAGEMENT_CLIENT_ID).get(0);
+
+        ClientResource accountResource = realm.clients().get(accountClient.getId());
+
+        RoleRepresentation manageAccountBasicAuth = accountResource.roles().get(AccountRoles.MANAGE_ACCOUNT_BASIC_AUTH).toRepresentation();
+        assertNotNull(manageAccountBasicAuth);
+
+        RoleRepresentation manageAccount2fa = accountResource.roles().get(AccountRoles.MANAGE_ACCOUNT_2FA).toRepresentation();
+        assertNotNull(manageAccount2fa);
     }
 
     protected void testRoleManageAccountLinks(RealmResource... realms) {
