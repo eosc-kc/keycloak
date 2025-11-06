9dc2614475b155971eeb28347639ef33217317a9
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2025-11-06 12:57:08 +0200

    Client Signature Required true requires also AuthnRequestsSigned be true

--- services/src/main/java/org/keycloak/protocol/saml/SamlProtocolFactory.java
+++ services/src/main/java/org/keycloak/protocol/saml/SamlProtocolFactory.java
@@ -207,7 +207,8 @@ public class SamlProtocolFactory extends AbstractLoginProtocolFactory {
             newClient.setFrontchannelLogout(true);
         }
 
-        client.setArtifactBindingIdentifierFrom(clientRep.getClientId());
+        if (clientRep.getClientId() != null)
+            client.setArtifactBindingIdentifierFrom(clientRep.getClientId());
     }
 
     /**
