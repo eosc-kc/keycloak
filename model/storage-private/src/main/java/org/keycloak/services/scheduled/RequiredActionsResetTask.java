4fd42e4a27df8af37031e73935e9f08b244b1106
Andreas Kozadinos <koza-sparrow@hotmail.com>
2026-01-30 15:12:35 +0200

    RCIAM-339 Terms and Conditions dynamic fields

--- model/storage-private/src/main/java/org/keycloak/services/scheduled/RequiredActionsResetTask.java
+++ model/storage-private/src/main/java/org/keycloak/services/scheduled/RequiredActionsResetTask.java
@@ -1,6 +1,8 @@
 package org.keycloak.services.scheduled;
 
 
+import java.util.Map;
+
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RequiredActionProviderModel;
@@ -16,6 +18,7 @@ public class RequiredActionsResetTask implements ScheduledTask {
 
     public static String INTERVAL_NUM = "reset_every";
     public static String UNIT_MULTIPLIER = "reset_every_multiplier";
+    //use 3600 for hours, 86400 for days, 2592000 for months
     protected final String realmId;
 
     public RequiredActionsResetTask(String realmId) {
@@ -34,12 +37,29 @@ public class RequiredActionsResetTask implements ScheduledTask {
         session.getContext().setRealm(realm);
         RequiredActionProviderModel requiredActionProviderModel = realm.getRequiredActionProviderByAlias(UserModel.RequiredAction.TERMS_AND_CONDITIONS.name());
 
-        if (requiredActionProviderModel != null && requiredActionProviderModel.isEnabled() && requiredActionProviderModel.getConfig().get(INTERVAL_NUM) != null && requiredActionProviderModel.getConfig().get(UNIT_MULTIPLIER) != null) {
-            session.users().searchUsersForRenewTermsAndConditions(realm, String.valueOf(System.currentTimeMillis() / 1000L)).forEach(user -> {
+        if (requiredActionProviderModel != null && requiredActionProviderModel.isEnabled() && requiredActionProviderModel.getConfig().get(INTERVAL_NUM) != null && !"0".equals(requiredActionProviderModel.getConfig().get(INTERVAL_NUM)) && requiredActionProviderModel.getConfig().get(UNIT_MULTIPLIER) != null) {
+            session.users().searchUsersForRenewTermsAndConditions(realm, calculateExpirationTime(requiredActionProviderModel.getConfig())).forEach(user -> {
                     user.addRequiredAction(UserModel.RequiredAction.TERMS_AND_CONDITIONS.name());
             });
         }
 
+    }
+
+    private String calculateExpirationTime(Map<String, String> config) {
+        return String.valueOf(System.currentTimeMillis() / 1000L - ( Long.valueOf(config.get(INTERVAL_NUM)) * getMultiplierFromUnit(config.get(UNIT_MULTIPLIER))));
+    }
+
+    private  long getMultiplierFromUnit(String unit) {
+        if (unit == null) return 86400L;
+
+        switch (unit.toLowerCase().trim()) {
+            case "hours":
+                return 3600L;
+            case "months":
+                return 2592000L;
+            default:
+                return 86400L;
+        }
     }
 
 }
