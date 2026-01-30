fffd7c3e7e0529763094351c3325050e633fab4d
Andreas Kozadinos <koza-sparrow@hotmail.com>
2026-01-30 15:12:35 +0200

    RCIAM-339 Terms and Conditions dynamic fields

--- services/src/main/java/org/keycloak/authentication/requiredactions/TermsAndConditions.java
+++ services/src/main/java/org/keycloak/authentication/requiredactions/TermsAndConditions.java
@@ -17,7 +17,9 @@
 
 package org.keycloak.authentication.requiredactions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import jakarta.ws.rs.core.Response;
 
@@ -29,6 +31,8 @@ import org.keycloak.common.util.Time;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.UserModel;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.services.messages.Messages;
 
 /**
@@ -38,6 +42,8 @@ import org.keycloak.services.messages.Messages;
 public class TermsAndConditions implements RequiredActionProvider, RequiredActionFactory {
     public static final String PROVIDER_ID = UserModel.RequiredAction.TERMS_AND_CONDITIONS.name();
     public static final String USER_ATTRIBUTE = "terms_and_conditions";
+    public static String INTERVAL_NUM = "reset_every";
+    public static String UNIT_MULTIPLIER = "reset_every_multiplier";
 
     @Override
     public RequiredActionProvider create(KeycloakSession session) {
@@ -63,6 +69,29 @@ public class TermsAndConditions implements RequiredActionProvider, RequiredActio
     @Override
     public void evaluateTriggers(RequiredActionContext context) {
 
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigMetadata() {
+        List<ProviderConfigProperty> config = new ArrayList<>(RequiredActionFactory.MAX_AUTH_AGE_CONFIG_PROPERTIES);
+
+        config.addAll(ProviderConfigurationBuilder.create()
+                .property()
+                .name(INTERVAL_NUM)
+                .label("resetInterval")
+                .helpText("resetIntervalHelp")
+                .type(ProviderConfigProperty.INTEGER_TYPE)
+                .add()
+                .property()
+                .name(UNIT_MULTIPLIER)
+                .label("resetIntervalMultiplier")
+                .helpText("resetIntervalMultiplierHelp")
+                .type(ProviderConfigProperty.LIST_TYPE)
+                .options(List.of("days","hours","months"))
+                .add()
+                .build());
+
+        return config;
     }
 
 
