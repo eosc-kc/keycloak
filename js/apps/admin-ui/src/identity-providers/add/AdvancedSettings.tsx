aa311e2fd01175f1c0c109fef938044dff198131
Andreas Kozadinos <koza-sparrow@hotmail.com>
2026-03-11 17:42:30 +0200

    Fixed HideOnLogin

--- js/apps/admin-ui/src/identity-providers/add/AdvancedSettings.tsx
+++ js/apps/admin-ui/src/identity-providers/add/AdvancedSettings.tsx
@@ -169,7 +169,7 @@ export const AdvancedSettings = ({
         fieldType="boolean"
       />
       <SwitchField
-        field="config.hideOnLogin"
+        field="hideOnLogin"
         label="hideOnLoginPage"
         fieldType="boolean"
       />
