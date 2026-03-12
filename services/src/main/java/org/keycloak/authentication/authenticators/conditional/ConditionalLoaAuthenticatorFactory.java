62dd468dc2733cbd6b715a0e5c643ef381db7058
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2026-03-12 12:21:05 +0200

    fix in ordering and overriden loa parameter

--- services/src/main/java/org/keycloak/authentication/authenticators/conditional/ConditionalLoaAuthenticatorFactory.java
+++ services/src/main/java/org/keycloak/authentication/authenticators/conditional/ConditionalLoaAuthenticatorFactory.java
@@ -53,9 +53,9 @@ public class ConditionalLoaAuthenticatorFactory implements ConditionalAuthentica
             .defaultValue(ConditionalLoaAuthenticator.DEFAULT_MAX_AGE) // 10 hours
             .add()
             .property()
-            .name(ConditionalLoaAuthenticator.CHECK_REQUIRED_LOA)
-            .label(ConditionalLoaAuthenticator.CHECK_REQUIRED_LOA)
-            .helpText(ConditionalLoaAuthenticator.CHECK_REQUIRED_LOA + ".tooltip")
+            .name(ConditionalLoaAuthenticator.OVERRIDEN_LOA)
+            .label(ConditionalLoaAuthenticator.OVERRIDEN_LOA)
+            .helpText(ConditionalLoaAuthenticator.OVERRIDEN_LOA + ".tooltip")
             .type(ProviderConfigProperty.BOOLEAN_TYPE)
             .add()
             .build();
