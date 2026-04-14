7340fcc246a8b30bd17baee6c015f9b374b4bf2b
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2026-04-14 15:32:31 +0300

    temp fix

--- services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
+++ services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
@@ -131,10 +131,9 @@ import org.keycloak.tracing.TracingAttributes;
 import org.keycloak.tracing.TracingProvider;
 import org.keycloak.util.JsonSerialization;
 import org.keycloak.util.TokenUtil;
+import org.keycloak.utils.StringUtil;
 
 import org.jboss.logging.Logger;
-
-import org.keycloak.utils.StringUtil;
 
 import static org.keycloak.OAuth2Constants.ORGANIZATION;
 import static org.keycloak.models.light.LightweightUserAdapter.isLightweightUser;
@@ -1386,7 +1385,7 @@ public class TokenManager {
             idToken.audience(client.getClientId());
             idToken.issuedNow();
             idToken.issuedFor(accessToken.getIssuedFor());
-            idToken.issuer(StringUtil.isNotBlank(realm.getIssuer()) ? : accessToken.getIssuer());
+            idToken.issuer(StringUtil.isNotBlank(realm.getIssuer()) ? realm.getIssuer() : accessToken.getIssuer());
             idToken.setNonce(clientSessionCtx.getAttribute(OIDCLoginProtocol.NONCE_PARAM, String.class));
             idToken.setSessionId(accessToken.getSessionId());
             idToken.exp(getIdTokenExpiration(accessToken.getExp()));
