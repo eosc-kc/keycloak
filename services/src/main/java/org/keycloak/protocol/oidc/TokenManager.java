24cee540cb595d71d7a544b5897b02fc6e39ef38
cgeorgilakis-grnet <cgeorgilakis@admin.grnet.gr>
2026-07-10 17:22:47 +0300

    remove empty scopes

--- services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
+++ services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
@@ -911,7 +911,7 @@ public class TokenManager {
     }
 
     public static Stream<String> parseScopeParameter(String scopeParam) {
-        return Arrays.stream(scopeParam.split(" ")).distinct();
+        return Arrays.stream(scopeParam.split(" ")).filter(String::isEmpty).distinct();
     }
 
     // Check if user still has granted consents to all requested client scopes
